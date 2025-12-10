b699b6e2e4dd21b73582cb0fa2ca69bcd714339f
JM Mac <johnmarkmancol@gmail.com>
2025-12-10 15:48:53 +0800

    chore: navigation icons

--- app/components/navigation/Navigation.vue
+++ app/components/navigation/Navigation.vue
@@ -11,42 +11,52 @@ const navigation = ref<Navigation[]>([
   {
     title: "Dashboard",
     path: "/",
+    icon: "i-lucide-layout-dashboard",
   },
   {
     title: "Catalog",
+    icon: "i-lucide-library",
     children: [
       {
         title: "Products",
         path: "/products",
+        icon: "i-lucide-layers",
       },
       {
         title: "Categories",
         path: "/categories",
+        icon: "i-lucide-chart-bar-stacked",
       },
       {
         title: "Attributes",
         path: "/attributes",
+        icon: "i-lucide-columns-3-cog",
       },
       {
         title: "Brands",
         path: "/brands",
+        icon: "i-lucide-tag",
       },
     ],
   },
   {
     title: "Transactions",
+    icon: "i-lucide-arrow-left-right",
     children: [
       {
         title: "Orders",
         path: "orders",
+        icon: "i-lucide-ticket-check",
       },
       {
         title: "Returns",
         path: "returns",
+        icon: "i-lucide-ticket-x",
       },
       {
         title: "Carts",
         path: "carts",
+        icon: "i-lucide-shopping-bag",
       },
     ],
   },
--- app/components/navigation/NavigationItem.vue
+++ app/components/navigation/NavigationItem.vue
@@ -1,6 +1,9 @@
 <template>
   <li v-for="(nav, k) in navigation" :key="`nav-item-${k}`">
-    <NuxtLink :to="nav.path" class="block py-1 px-2">{{ nav.title }}</NuxtLink>
+    <NuxtLink :to="nav.path" class="flex items-center gap-2 py-1 px-2">
+      <UIcon v-if="nav.icon" :name="nav.icon" class="size-4 text-gray-400" />
+      {{ nav.title }}
+    </NuxtLink>
     <ul v-if="nav.children?.length" class="pl-2 ml-4 border-l border-gray-200">
       <NavigationItem :navigation="nav.children" />
     </ul>
