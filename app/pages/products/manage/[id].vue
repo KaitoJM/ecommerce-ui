c0c9aa60e97bd03a50432df09ca0a97db5100c75
JM Mac <johnmarkmancol@gmail.com>
2025-12-12 14:48:46 +0800

    product manage tabs

--- app/pages/products/manage/[id].vue
+++ app/pages/products/manage/[id].vue
@@ -1,5 +1,67 @@
 <template>
-  <div>sdfds</div>
+  <div>
+    <UTabs :items="tabItems">
+      <template #information>
+        <UForm :state="state" class="flex flex-col gap-4">
+          <UFormField label="Name" name="name">
+            <UInput v-model="state.name" class="w-full" />
+          </UFormField>
+          <UFormField label="Username" name="username">
+            <UInput v-model="state.username" class="w-full" />
+          </UFormField>
+        </UForm>
+      </template>
+
+      <template #pricing>
+        <UForm :state="state" class="flex flex-col gap-4">
+          <UFormField label="Current Password" name="current" required>
+            <UInput
+              v-model="state.currentPassword"
+              type="password"
+              required
+              class="w-full"
+            />
+          </UFormField>
+          <UFormField label="New Password" name="new" required>
+            <UInput
+              v-model="state.newPassword"
+              type="password"
+              required
+              class="w-full"
+            />
+          </UFormField>
+          <UFormField label="Confirm Password" name="confirm" required>
+            <UInput
+              v-model="state.confirmPassword"
+              type="password"
+              required
+              class="w-full"
+            />
+          </UFormField>
+        </UForm>
+      </template>
+      <template #images>
+        <UForm :state="state" class="flex flex-col gap-4">
+          <UFormField label="Name" name="name">
+            <UInput v-model="state.name" class="w-full" />
+          </UFormField>
+          <UFormField label="Username" name="username">
+            <UInput v-model="state.username" class="w-full" />
+          </UFormField>
+        </UForm>
+      </template>
+      <template #combination>
+        <UForm :state="state" class="flex flex-col gap-4">
+          <UFormField label="Name" name="name">
+            <UInput v-model="state.name" class="w-full" />
+          </UFormField>
+          <UFormField label="Username" name="username">
+            <UInput v-model="state.username" class="w-full" />
+          </UFormField>
+        </UForm>
+      </template>
+    </UTabs>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -10,8 +72,34 @@ definePageMeta({
 });
 
 const navigationStore = useNavigationStore();
+const tabItems = [
+  {
+    label: "Product Information",
+    slot: "information",
+  },
+  {
+    label: "Pricing",
+    slot: "pricing",
+  },
+  {
+    label: "Images",
+    slot: "images",
+  },
+  {
+    label: "Combinations",
+    slot: "combination",
+  },
+];
 
 onMounted(() => {
   navigationStore.setPageTitle("Manage Product");
+});
+
+const state = reactive({
+  name: "Benjamin Canac",
+  username: "benjamincanac",
+  currentPassword: "",
+  newPassword: "",
+  confirmPassword: "",
 });
 </script>
