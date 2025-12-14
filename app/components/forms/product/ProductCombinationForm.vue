472379dd2789a71f7f053ae6a46647aae3b8c58d
JM Mac <johnmarkmancol@gmail.com>
2025-12-14 18:36:50 +0800

    chore add product attribute

--- app/components/forms/product/ProductCombinationForm.vue
+++ app/components/forms/product/ProductCombinationForm.vue
@@ -2,18 +2,54 @@
   <div class="flex flex-col gap-4">
     <div class="flex gap-4 mt-4">
       <div class="w-80 flex flex-col gap-4">
-        <UFormField label="Attribute">
-          <UFieldGroup class="w-full">
-            <USelectMenu placeholder="Attributes" :items="attributeItems" />
-            <UInput placeholder="Attribute value" />
-            <UButton
-              color="neutral"
-              variant="outline"
-              icon="i-lucide-plus"
-              label="Add"
-            />
-          </UFieldGroup>
-        </UFormField>
+        <UForm
+          @submit.prevent="handleProductAttributeSubmit"
+          class="flex flex-col gap-2"
+        >
+          <UFormField label="Attribute" class="w-full">
+            <UFieldGroup class="w-full">
+              <USelectMenu
+                v-model="productAttributeForm.attribute_id"
+                placeholder="Attributes"
+                value-key="value"
+                :items="attributeItems"
+              />
+              <UInput
+                v-model="productAttributeForm.value"
+                placeholder="Attribute value"
+                class="w-full"
+              />
+              <UPopover v-if="isTypeColor()">
+                <UTooltip text="Select color value">
+                  <UButton color="neutral" variant="outline">
+                    <template #leading>
+                      <span
+                        :style="`background-color: ${productAttributeForm.color_value}`"
+                        class="size-5 rounded"
+                      />
+                    </template>
+                  </UButton>
+                </UTooltip>
+
+                <template #content>
+                  <UColorPicker
+                    v-model="productAttributeForm.color_value"
+                    class="p-2"
+                  />
+                </template>
+              </UPopover>
+            </UFieldGroup>
+          </UFormField>
+          <UButton
+            :loading="addingAttribute"
+            type="submit"
+            color="primary"
+            variant="outline"
+            icon="i-lucide-plus"
+            label="Add this attribute to product"
+            class="flex justify-center"
+          />
+        </UForm>
         <p class="text-xs">
           If you did not see your prefered attribute from the list, You can add
           new by clicking
@@ -75,21 +111,71 @@
 </template>
 
 <script setup lang="ts">
+import type { FetchError } from "ofetch";
 import { useAttributeStore } from "~/store/attribute.store";
 import { useProductFormStore } from "~/store/productForm.store";
-import { useProductFormAttributeStore } from "~/store/productFormAttribute.store";
+import {
+  useProductFormAttributeStore,
+  type ProductAttributeForm,
+} from "~/store/productFormAttribute.store";
 import type { ProductAttribute } from "~/types/Product.types";
 
 const productFormStore = useProductFormStore();
 const productFormAttributeStore = useProductFormAttributeStore();
 const attributeStore = useAttributeStore();
+const toast = useToast();
 
+const productId = computed(() => productFormStore.product?.id);
 const attributeItems = computed(() =>
-  attributeStore.attributes.map((attr) => attr.attribute)
+  attributeStore.attributes.map((attr) => {
+    return {
+      label: attr.attribute,
+      value: attr.id,
+    };
+  })
 );
 const productAttributes = computed(
   () => productFormAttributeStore.productAttributes
 );
-
 attributeStore.getAttributes({ page: 1, per_page: 100 });
+
+const productAttributeForm: ProductAttributeForm = reactive({
+  product_id: productId.value as string,
+  attribute_id: "",
+  value: "",
+  color_value: "red",
+});
+
+const isTypeColor = (): boolean => {
+  const selectedAttribute = productAttributeForm.attribute_id;
+
+  if (
+    attributeStore.attributes.find((attr) => attr.id == selectedAttribute)
+      ?.attribute == "Color"
+  ) {
+    return true;
+  }
+
+  return false;
+};
+
+const addingAttribute = computed(() => productFormAttributeStore.adding);
+const handleProductAttributeSubmit = async () => {
+  try {
+    await productFormAttributeStore.createProductAttribute(
+      productAttributeForm
+    );
+  } catch (error) {
+    const fetchError = error as FetchError<any>;
+    toast.add({
+      title: "Error",
+      description:
+        fetchError.data?.message ??
+        fetchError.message ??
+        "Something went wrong",
+      icon: "i-lucide-octagon-x",
+      color: "error",
+    });
+  }
+};
 </script>
--- app/store/productFormAttribute.store.ts
+++ app/store/productFormAttribute.store.ts
@@ -4,6 +4,13 @@ import type { ProductAttribute } from "~/types/Product.types";
 
 const config = useRuntimeConfig();
 
+export interface ProductAttributeForm {
+  product_id: string;
+  attribute_id: string;
+  value: string;
+  color_value?: string;
+}
+
 export const useProductFormAttributeStore = defineStore(
   "productFormAttributeStore",
   () => {
@@ -59,9 +66,63 @@ export const useProductFormAttributeStore = defineStore(
       }
     };
 
+    const adding = ref<boolean>(false);
+    const createProductAttribute = async (
+      params: ProductAttributeForm
+    ): Promise<ProductAttribute> => {
+      const token = localStorage.getItem("token");
+      adding.value = true;
+
+      if (!token) {
+        console.error(
+          `Failed to get product attributes.`,
+          "No auth token found"
+        );
+        throw {
+          message: "Authentication required. Please log in again.",
+          statusCode: 401,
+        } satisfies ApiError;
+      }
+
+      try {
+        const response: ApiSuccess<ProductAttribute> = await $fetch(
+          `${config.public.apiBase}/product-attributes`,
+          {
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${token}`,
+              Accept: "application/json",
+            },
+            body: params,
+          }
+        );
+
+        adding.value = false;
+        getProductAttributes(params.product_id);
+        return response.data;
+      } catch (error) {
+        adding.value = false;
+        const fetchError = error as FetchError<any>;
+
+        const apiError: ApiError = {
+          message:
+            fetchError.data?.message ??
+            fetchError.message ??
+            "Something went wrong",
+          errors: fetchError.data?.errors,
+          statusCode: fetchError.status,
+        };
+
+        console.error(`Failed to create new product attribute:`, error);
+        throw apiError;
+      }
+    };
+
     return {
       productAttributes,
       getProductAttributes,
+      adding,
+      createProductAttribute,
     };
   }
 );
