3b882193811dc4a4ba0843431bbf5b15b66c729d
JM Mac <johnmarkmancol@gmail.com>
2025-12-15 14:29:43 +0800

    chore: added brands page

---
+++ app/pages/brands/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque ab assumenda
+    quae nostrum rerum vel harum, nisi mollitia, modi repellendus voluptatum
+    tempora excepturi vitae dicta quia officiis dolorem ipsum reprehenderit!
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: "main-template",
+  middleware: "auth",
+});
+</script>
