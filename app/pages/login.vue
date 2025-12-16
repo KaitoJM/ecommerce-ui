02fd568ccab40b98ed6ce900a1d6d81439c25599
JM Mac <johnmarkmancol@gmail.com>
2025-12-17 00:05:57 +0800

    fix: login page

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -3,6 +3,7 @@
     <Logo class="w-60" />
     <UPageCard class="w-full max-w-md">
       <UForm class="flex flex-col gap-4" @submit.prevent="handleLogin">
+        <p class="font-bold text-lg">Welcome back!</p>
         <UFormField label="Email">
           <UInput
             v-model="email"
@@ -14,17 +15,37 @@
           <UInput
             v-model="password"
             placeholder="Enter your password"
-            type="password"
             class="w-full"
-          />
+            :type="showPassword ? 'text' : 'password'"
+            :ui="{ trailing: 'pe-1' }"
+          >
+            <template #trailing>
+              <UButton
+                color="neutral"
+                variant="link"
+                size="sm"
+                :icon="showPassword ? 'i-lucide-eye-off' : 'i-lucide-eye'"
+                :aria-label="showPassword ? 'Hide password' : 'Show password'"
+                :aria-pressed="showPassword"
+                aria-controls="password"
+                @click="showPassword = !showPassword"
+              />
+            </template>
+          </UInput>
         </UFormField>
-        <UButton type="submit" class="mt-4 flex justify-center">Login</UButton>
+        <UButton
+          :loading="loading"
+          type="submit"
+          class="mt-4 flex justify-center"
+          >Login</UButton
+        >
       </UForm>
     </UPageCard>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { FetchError } from "ofetch";
 import Logo from "~/components/ui/Logo.vue";
 
 import { ref } from "vue";
@@ -38,10 +59,13 @@ interface LoginResponse {
 const config = useRuntimeConfig();
 const router = useRouter();
 
-const email = ref<string>("");
-const password = ref<string>("");
+const email = ref<string>("test@example.com");
+const password = ref<string>("password");
 const loading = ref<boolean>(false);
 const errorMessage = ref<string>("");
+const toast = useToast();
+
+const showPassword = ref(false);
 
 const handleLogin = async () => {
   loading.value = true;
@@ -60,15 +84,25 @@ const handleLogin = async () => {
     localStorage.setItem("user", JSON.stringify(res.user));
     router.push("/");
   } catch (err) {
-    const error = err as {
-      statusCode?: number;
-      data?: {
-        message: string;
-      };
-    };
+    const fetchError = err as FetchError<any>;
 
-    if (error?.statusCode == 422) {
-      errorMessage.value = error?.data?.message || "";
+    if (fetchError.statusCode == 401) {
+      toast.add({
+        title: "Invalid Credentials",
+        description: "Your email or password is incorrect",
+        icon: "i-lucide-lock",
+        color: "error",
+      });
+    } else {
+      toast.add({
+        title: "Error",
+        description:
+          fetchError.data?.message ??
+          fetchError.message ??
+          "Something went wrong",
+        icon: "i-lucide-octagon-x",
+        color: "error",
+      });
     }
   } finally {
     loading.value = false;
