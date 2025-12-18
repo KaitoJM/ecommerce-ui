223f792936f056afbab6cc790e61e95717fd31ab
JM Mac <johnmarkmancol@gmail.com>
2025-12-18 21:47:51 +0800

    fix: console warnings in category page

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,17 @@
 <template>
-  <UApp toaster>
+  <UApp :toaster="toasterConfig">
     <NuxtLoadingIndicator />
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
   </UApp>
 </template>
+
+<script setup lang="ts">
+import type { ToasterProps } from "@nuxt/ui";
+
+const toasterConfig: ToasterProps = {
+  duration: 5000,
+  position: "bottom-right",
+};
+</script>
