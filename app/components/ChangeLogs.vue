1f4308f34cf3e47ed67c0625d6ceda0ed7113369
JM Mac <johnmarkmancol@gmail.com>
2025-12-20 01:27:10 +0800

    chore: new change log layout

--- app/components/ChangeLogs.vue
+++ app/components/ChangeLogs.vue
@@ -7,16 +7,33 @@
           v-for="(release, releaseKey) in changeLogs"
           :key="`release-${releaseKey}`"
         >
-          <p>{{ release.date }}</p>
-          <ul v-if="release.commits.length">
-            <li
-              v-for="(commit, commitKey) in release.commits"
-              :key="`commit-${commitKey}`"
+          <div class="flex items-center gap-2">
+            <div class="h-4 w-4 block rounded-full bg-primary"></div>
+            <div class="flex items-center gap-2">
+              <p class="text-xs">{{ formatDate(release.date) }}</p>
+              <UBadge :label="release.version" variant="outline" />
+            </div>
+          </div>
+          <div class="pl-8 border-l border-primary ml-2">
+            <ul
+              v-if="release.commits.length"
+              class="bg-neutral-100 dark:bg-neutral-800 p-4 rounded-lg mt-4 flex flex-col gap-4"
             >
-              <p>{{ commit.date }} - {{ commit.message }}</p>
-              <p>{{ commit.author }}</p>
-            </li>
-          </ul>
+              <li
+                v-for="(commit, commitKey) in release.commits"
+                :key="`commit-${commitKey}`"
+              >
+                <p class="text-xs">{{ formatDate(commit.date) }}</p>
+                <p class="py-2 font-bold text-sm">{{ commit.message }}</p>
+                <div class="flex items-center gap-2">
+                  <UAvatar :src="commit.avatar" class="size-4" />
+                  <p class="text-xs text-neutral-500">
+                    Commited by {{ commit.author }}
+                  </p>
+                </div>
+              </li>
+            </ul>
+          </div>
         </li>
       </ul>
     </div>
